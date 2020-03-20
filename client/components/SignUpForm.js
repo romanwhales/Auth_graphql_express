9e49b01d07b15902694ec45808a8f06f86727cf5
Olawale Sunmonu-Adedeji <olawalesunmonu-adedeji@Olawales-MacBook-Pro.local>
2020-03-20 07:57:53 +0100

    Graphql initial commit

---
+++ client/components/SignUpForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AuthForm from './AuthForm';
+
+class SignUpForm extends Component {
+  render() {
+    return (
+      <div>
+        <h3>SignUpForm</h3>
+        <AuthForm />
+      </div>
+    );
+  }
+}
+
+export default SignUpForm;
